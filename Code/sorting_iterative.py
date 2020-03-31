c62d2b1a78e75df79533ae5d05f8e787f8debbb6
liyaSileshi <liyasileshi@gmail.com>
2020-03-31 00:28:53 -0700

    is_sorted done

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -5,8 +5,13 @@ def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check that all adjacent items are in order, return early if so
-
+    #Check that all adjacent items are in order, return early if so
+    if len(items) < 2: #if there is only 1 item or none
+        return True
+    for i in range(len(items) - 1):
+        if items[i] > items[i+1]:
+            return False #not sorted
+    return True #sorted
 
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
@@ -35,3 +40,7 @@ def insertion_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
+
+
+if __name__ == '__main__':
+    print(is_sorted(['a', 'b', 'c', 'd']))
