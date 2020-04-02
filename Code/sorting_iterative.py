c1e0725fd09dce6d79694db6485083b35b211e16
liyaSileshi <liyasileshi@gmail.com>
2020-04-01 20:13:14 -0700

    updated bubble sort

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -8,30 +8,42 @@ def is_sorted(items):
     #Check that all adjacent items are in order, return early if so
     if len(items) < 2: #if there is only 1 item or none
         return True
+
     for i in range(len(items) - 1):
         if items[i] > items[i+1]:
             return False #not sorted
     return True #sorted
 
+def swap(items, i, j): 
+    '''Helper function to swap items'''
+    # temp = items[j]
+    # items[j] = items[j+1]
+    # items[j+1] = temp
+    items[i], items[j] = items[j], items[i] 
+
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
+    Running time: Worst case: O(n^2) - we'll have O(n-1) passes
+                                        and checking to swap each element with
+                                        the rest of the elements O(n)
+                  Best case: O(n) - when the items are sorted. 
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Swap adjacent items that are out of order
+    #Repeat until all items are in sorted order
+    #Swap adjacent items that are out of order
 
     steps = 0 #to track the range of items we want to compare
-    looks = len(items) - steps 
-
+    looks = len(items) - steps #the range of item we'll compare
+    swapped = False #to keep track if any swaps are being made in one round loop
     for _ in range(looks):
         for j in range(looks - 1): #-1 for index not to be out of range
             if items[j] > items[j+1]:
-                temp = items[j]
-                items[j] = items[j+1]
-                items[j+1] = temp
+                swap(items, j, j+1) #call the swap function
+                swapped = True #we made a swap
+                
         steps -= 1 #decrements because each iteration, the last item will be sorted
-
+        if swapped == False: #no swaps were made; everything is sorted
+            return items #exit early
     return items
     
 def selection_sort(items):
@@ -42,6 +54,10 @@ def selection_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Find minimum item in unsorted items
     # TODO: Swap it with first unsorted item
+    pointer = 0
+    minimum = items[pointer]
+    for i in items:
+        tracker_minimum = i 
 
 
 def insertion_sort(items):
