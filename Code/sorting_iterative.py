69c20e1d9256a0286b0334ddb1115edf7ba6bfd1
liyaSileshi <liyasileshi@gmail.com>
2020-04-01 21:56:50 -0700

    selection sort done

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -16,9 +16,6 @@ def is_sorted(items):
 
 def swap(items, i, j): 
     '''Helper function to swap items'''
-    # temp = items[j]
-    # items[j] = items[j+1]
-    # items[j+1] = temp
     items[i], items[j] = items[j], items[i] 
 
 def bubble_sort(items):
@@ -44,21 +41,32 @@ def bubble_sort(items):
         steps -= 1 #decrements because each iteration, the last item will be sorted
         if swapped == False: #no swaps were made; everything is sorted
             return items #exit early
+
     return items
     
 def selection_sort(items):
     """Sort given items by finding minimum item, swapping it with first
     unsorted item, and repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Find minimum item in unsorted items
-    # TODO: Swap it with first unsorted item
-    pointer = 0
-    minimum = items[pointer]
-    for i in items:
-        tracker_minimum = i 
+    Running time: O(n^2) even if all items are sorted we would have to loop 
+                    through the entire array to check if there's minimum than
+                    current...
 
+    TODO: Memory usage: ??? Why and under what conditions?"""
+    # Repeat until all items are in sorted order
+    # Find minimum item in unsorted items
+    # Swap it with first unsorted item
+    
+    for i in range(len(items)): 
+        curr_min = items[i] #should hold the minimum elt from the given range (updates to find the 'real' minimum)
+        min_index = i #the index of the minimum
+        for j in range(i+1, len(items)): #the items in the right of the curr_min
+            if items[j] < curr_min: #compare the curr_min to each item
+                curr_min = items[j] #if lesser is found, update curr_min
+                min_index = j #update the index as well
+
+        if min_index != i: #to make sure curr min was actually updated
+            swap(items, min_index, i) #swap the new min with old min(i)
+    return items 
 
 def insertion_sort(items):
     """Sort given items by taking first unsorted item, inserting it in sorted
@@ -72,4 +80,5 @@ def insertion_sort(items):
 
 if __name__ == '__main__':
     # print(is_sorted(['a', 'b', 'c', 'd']))
-    print(bubble_sort([5,3]))
+    # print(bubble_sort([5,3]))
+    print(selection_sort([5,3]))
